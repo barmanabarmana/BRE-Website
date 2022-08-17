cba52a5f66e9df44a54fa4fdb9328778060db626
barmanabarmana <barmana2003@gmail.com>
2022-08-17 22:32:04 +0200

    Added adapter-static + build folder

--- webPage/svelte.config.js
+++ webPage/svelte.config.js
@@ -1,10 +1,24 @@
-import adapter from '@sveltejs/adapter-auto';
-
+import adapter from "@sveltejs/adapter-static";
+import { svelte } from '@sveltejs/vite-plugin-svelte'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		prerender: {
+			default: true,
+		}
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				entryFileNames: '[name].js',
+				assetFileNames: '[name].[ext]',
+			},
+		},
+	},
+	plugins: [
+		svelte(),
+	],
 };
 
 export default config;
